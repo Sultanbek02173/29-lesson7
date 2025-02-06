c3e092d5bf30cb99f759d128eacb793bddde3dfc
Sultanbek02173 <sultan.02173@gmail.com>
2025-02-06 21:31:01 +0600

    lesson 7

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [data, setData] = useState();
+  const [newComment, setNewComment] = useState({name: '', text: ''})
+  const [changeComment, setChangeComment] = useState({name: '', text: ''})
+  const [changeId, setChangeId] = useState(null);
+
+  const getApi = () => {
+    axios.get('http://localhost:5000/comments')
+    .then(({data}) => setData(data))
+    .catch((error) => console.log(error))
+  }
+
+  const postApi = (newCom) => {
+    axios.post('http://localhost:5000/comments', newCom)
+    .then(() =>{
+      getApi()
+      setNewComment({name: '', text: ''})
+    })
+    .catch((error) => console.log(error))
+  }
+
+  const deleteApi = (id) => {
+    axios.delete(`http://localhost:5000/comments/${id}`)
+    .then(() => getApi())
+    .catch((error) => console.log(error))
+  }
+
+  const patchApi = (id, changeComment) => {
+    axios.patch(`http://localhost:5000/comments/${id}`, changeComment)
+    .then(() => {getApi()})
+    .catch((error) => console.log(error));
+  } 
+
+  useEffect(() => {
+    getApi();
+  }, [])
+
+  return (
+    <div>
+      <h1>Коментарии</h1>
+      {
+        data && 
+        data.map((comments) => (
+          <div key={comments.id}>
+            {changeId === comments.id ? (
+              <div>
+                <input 
+                  type="text" 
+                  value={changeComment.name} 
+                  onChange={(e) => setChangeComment({...changeComment, name: e.target.value})} 
+                />
+
+                <input 
+                  type="text" 
+                  value={changeComment.text} 
+                  onChange={(e) => setChangeComment({...changeComment, text: e.target.value})} 
+                />
+                <button onClick={() => {
+                  patchApi(comments.id, changeComment)
+                  setChangeId(null)
+                  }}>Access</button>
+                <button onClick={() => setChangeId(null)}>Cancel</button>
+              </div>
+            ) : (
+              <div>
+                <h2>{comments.name}</h2>
+                <p>{comments.text}</p>
+
+                <button onClick={() => {
+                  setChangeId(comments.id)
+                  setChangeComment({name: comments.name, text: comments.text})
+                  }}>Change text</button>
+
+                <button onClick={() => deleteApi(comments.id)}>Delete</button>
+              </div>
+            )}
+          </div>
+        ))
+      }
+      <input 
+        type="text" 
+        value={newComment.name} 
+        onChange={(e) => setNewComment({...newComment, name: e.target.value})} 
+      />
+
+      <input 
+        type="text" 
+        value={newComment.text} 
+        onChange={(e) => setNewComment({...newComment, text: e.target.value})} 
+      />
+      <button onClick={() => {postApi(newComment)}}>Send comment</button>
+    </div>
+  );
+}
+
+export default App;
